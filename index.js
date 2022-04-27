72d0c2bc6a069be191ad07630b51c9bf8d78bd92
MMaidul <maidulmuqtadir360@gmail.com>
2022-04-27 09:26:33 +0600

    created genius car server

---
+++ index.js
@@ -0,0 +1,72 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+//middleware;
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbbc8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run (){
+try{
+    await client.connect();
+    const serviceCollection =  client.db('geniusCar').collection('service');
+    //getAllServiceFromMongodb
+    app.get('/service',async(req,res)=>{
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    })
+
+    //getSingleServiceFromMongodb;
+    app.get('/service/:id',async(req,res)=>{
+        console.log('service');
+        const id =  req.params.id;
+        const query = {_id: ObjectId(id)};
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+
+    })
+
+    
+    app.post('/service',async(req,res)=>{
+        const newService = req.body;
+        const result = await serviceCollection.insertOne(newService);
+        res.send(result); 
+    })
+
+    app.delete('/service/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result)
+    })
+
+}
+finally{
+
+}
+}
+
+run().catch(console.dir)
+
+
+app.get('/',(req,res)=>{
+    res.send('Genius Server is Running');
+})
+
+app.listen(port, ()=>{
+    console.log(`Data Server port: ${port}`);
+})
+
+
+
